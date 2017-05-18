cf3b3eba0315081d0f057bfc5f7b07cbeffd6000
korn <kornchaval@gmail.com>
2017-05-18 16:24:06 +0700

    Config Express and Enviroment Dev or Production

--- config/express.js
+++ config/express.js
@@ -1,11 +1,26 @@
 var express = require('express');
-
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
 /*
 https://www.youtube.com/watch?v=93GfL42eUv0&list=PLtM3znnbMbVXD0fygCTsblC2sLZvSPY8g&index=19
 อธิบายการทำงาน
 */
 module.exports = function() {
   var app = express();
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else {
+    app.use(compression('production'));
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+
   require('../app/routes/index.routes')(app);
   return app;
 
